504c77434d2ec204013d983f6648840da6af4da8
Matthew Moore <m3moore@gmail.com>
2017-12-23 16:49:42 -0800

    Initial Commit

---
+++ OpenCVTensorflowExample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTensorflowExample
+//
+//  Created by ThriveSmart One on 12/23/17.
+//  Copyright © 2017 Greppy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
